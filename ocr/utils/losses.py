31200f5a9fdca9c56d694a4c1cd96451ffc4e6f0
morestrt <morestarforyou@gmail.com>
2021-07-31 18:45:27 +0800

    修改CTC loss

--- ocr/utils/losses.py
+++ ocr/utils/losses.py
@@ -1,13 +1,26 @@
 import torch.nn as nn
 
 from . import base
+import torch
 
 
 __all__ = ['get_loss']
 
 
-class CTCLoss(nn.CTCLoss, base.Loss):
-    pass
+class CTCLoss(nn.Module):
+
+    def __init__(self, blank_idx, reduction='mean'):
+        super().__init__()
+        self.loss_func = torch.nn.CTCLoss(blank=blank_idx, reduction=reduction, zero_infinity=True)
+
+    def forward(self, pred, args):
+        batch_size = pred.size(0)
+        label, label_length = args['targets'], args['targets_lengths']
+        pred = pred.log_softmax(2)
+        pred = pred.permute(1, 0, 2)
+        pred_lengths = torch.tensor([pred.size(0)] * batch_size, dtype=torch.long)
+        loss = self.loss_func(pred, label, pred_lengths, label_length)
+        return {'loss': loss}
 
 
 def get_loss(name: str = 'ctc'):
@@ -18,4 +31,4 @@ def get_loss(name: str = 'ctc'):
             f'loss name must be {all_loss}, got `{name}`'
         )
     if loss_name == 'ctc':
-        return CTCLoss(blank=0)
+        return CTCLoss(blank_idx=0)
