ab6f18116497258fc289676c1aceff3ad078b20e
cat <morestarforyou@gmail.com>
2021-11-26 16:47:26 +0800

    训练部分code封装

--- tools/train/det_train/train.py
+++ tools/train/det_train/train.py
@@ -1,59 +1,118 @@
-# @Time    : 2021/9/18 下午5:53
-# @Author  : 
-# @File    : train
-# @Software: PyCharm
-# @explain :
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
-from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.plugins import DDPPlugin
-from pytorch_lightning.callbacks import LearningRateMonitor
-from pytorch_lightning.callbacks import RichProgressBar
-from det import DBDetModel, DetDataModule
 import pytorch_lightning as pl
-import logging
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor, RichProgressBar
+from pytorch_lightning.loggers import WandbLogger
 
-logging.getLogger("lightning").setLevel(logging.ERROR)
-pl.seed_everything(1997)
+from det import DBDetModel, DetDataModule
 
-model = DBDetModel(
-    encoder_name='dpn92',
-    lr=0.01,
-    optimizer_name='sgd',
-    weights='imagenet+5k',
-    finetune=False
-)
 
-data = DetDataModule(
-    train_data_path='/home/cat/Documents/ICDAR/ICDAR2019/train.json',
-    val_data_path='/home/cat/Documents/ICDAR/ICDAR2017/val.json',
-    batch_size=8,
-    num_workers=8
-)
+class TrainDB:
+    def __init__(
+            self,
+            train_data_path: str,
+            val_data_path: str,
+            checkpoint_save_path: str,
+            encoder_name: str = 'dpn68',
+            lr: float = 0.001,
+            batch_size: int = 16,
+            num_workers: int = 16,
+            optimizer_name: str = 'sgd',
+            weights: str = 'imagenet',
+            pretrained_path: str = '',
+    ):
+        pl.seed_everything(1997)
+        self.bs = batch_size
+        self.nw = num_workers
+        self.checkpoint_save_path = checkpoint_save_path
+        self.train_data_path = train_data_path
+        self.val_data_path = val_data_path
+        self.encoder_name = encoder_name
+        self.lr = lr
+        self.optimizer_name = optimizer_name
+        self.weights = weights
+        self.pretrained_path = pretrained_path
 
-logger = WandbLogger()
+    def build_model(self):
+        return DBDetModel(
+            encoder_name=self.encoder_name,
+            lr=self.lr,
+            optimizer_name=self.optimizer_name,
+            weights=self.weights,
+            pretrained_model_path=self.pretrained_path,
+        )
 
-early_stop = EarlyStopping(patience=20, monitor='hmean', mode='max')
-checkpoint_callback = ModelCheckpoint(
-    monitor='hmean',
-    mode='max',
-    dirpath='/home/cat/PycharmProjects/torch-ocr/tools/train/weights',
-    filename='DB-' + model.encoder_name + '-{epoch:02d}-{hmean:.2f}-{recall:.2f}-{precision:.2f}',
-    save_last=True,
-)
+    def load_model(self):
+        model = self.build_model()
+        if model.pretrained_model_path:
+            model.load_from_checkpoint(model.pretrained_model_path)
+            return model
+        else:
+            return model
 
-lr_monitor = LearningRateMonitor(logging_interval='epoch')
-rp = RichProgressBar(leave=True)
+    def load_datamodule(self):
+        return DetDataModule(
+            train_data_path=self.train_data_path,
+            val_data_path=self.val_data_path,
+            batch_size=self.bs,
+            num_workers=self.nw,
+        )
 
-# DP 一机多卡
-trainer = pl.Trainer(
-    # open this, must drop last
-    # benchmark=True,
-    # gpus=2, strategy="ddp",
-    gpus=[1],
-    max_epochs=100,
-    min_epochs=80,
-    logger=[logger],
-    callbacks=[early_stop, checkpoint_callback, lr_monitor, rp],
-)
+    def build_trainer(
+            self,
+            gpus: list,
+            max_epochs: int,
+            min_epochs: int,
+    ):
+        if len(gpus) >= 2:
+            strategy = "ddp"
+            gpus = len(gpus)
+        else:
+            strategy = None
 
-trainer.fit(model, data)
+        model = self.load_model()
+        data = self.load_datamodule()
+
+        logger = WandbLogger()
+
+        early_stop = EarlyStopping(patience=20, monitor='hmean', mode='max')
+        checkpoint_callback = ModelCheckpoint(
+            monitor='hmean',
+            mode='max',
+            dirpath=self.checkpoint_save_path,
+            filename='DB-' + model.encoder_name + '-{epoch:02d}-{hmean:.2f}-{recall:.2f}-{precision:.2f}',
+            save_last=True,
+        )
+
+        lr_monitor = LearningRateMonitor(logging_interval='epoch')
+        rp = RichProgressBar(leave=True)
+
+        trainer = pl.Trainer(
+            gpus=gpus,
+            strategy=strategy,
+            max_epochs=max_epochs,
+            min_epochs=min_epochs,
+            logger=logger,
+            callbacks=[early_stop, checkpoint_callback, lr_monitor, rp]
+        )
+
+        trainer.fit(model, data)
+
+
+if __name__ == '__main__':
+
+    db = TrainDB(
+        train_data_path='/home/cat/Documents/ZF/train.json',
+        val_data_path='/home/cat/Documents/ZF/val.json',
+        checkpoint_save_path='../weights',
+        encoder_name='resnet50',
+        weights='imagenet',
+        lr=0.01,
+        batch_size=16,
+        num_workers=16,
+        optimizer_name='sgd',
+        pretrained_path=''
+    )
+    db.build_trainer(
+        gpus=[1],
+        max_epochs=150,
+        min_epochs=80,
+    )
